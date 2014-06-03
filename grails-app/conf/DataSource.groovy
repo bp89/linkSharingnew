a4ce9ff095055c8cba6b60893d8a61ae91376081
bp89 <banti.prajapati@gmail.com>
2014-06-03 16:22:19 +0530

    New database configuration

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -40,8 +40,12 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/banti_prajapati"
+            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+            username = "root"
+            password = "igdefault"
+
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
