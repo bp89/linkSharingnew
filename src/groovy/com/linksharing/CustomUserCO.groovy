054b2386f2b142f2a333a3a04f2ad4fb730727da
bp89 <banti.prajapati@gmail.com>
2014-06-17 10:54:55 +0530

    Admin related changes

---
+++ src/groovy/com/linksharing/CustomUserCO.groovy
@@ -0,0 +1,17 @@
+package com.linksharing
+
+import linksharing.User
+
+/**
+ * Created by prajapati on 16/6/14.
+ */
+class CustomUserCO {
+    Long id
+    String name
+
+
+    CustomUserCO(Long id,String name){
+        this.id=id
+        this.name=name
+    }
+}
